40abcbeaf5962c44121d52b419b75acb89094647
Shashank <54907190+shashank8496@users.noreply.github.com>
2019-12-31 17:18:49 +0530

    Add files via upload

---
+++ loaddataset.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 14:19:27 2019
+
+@author: Vamshi
+"""
+import pymongo as pm
+
+connection = pm.MongoClient()
+heart = connection.heart
+heart_collection = heart.heart_data
+heart_collection.drop()
+
+def import_content(filename):
+    print('Reading file...')
+    data = open(filename, 'r')
+    header = True
+    keys = []
+    step = 0
+    batch_data = []
+    for line in data:
+        line_data = {}
+        if header:
+            keys = line.split(',')
+            print(keys)
+            header = False
+        else:
+            line = line.split(',')
+            for k in range(len(keys)):
+                line_data[keys[k]] = line[k]
+            if step % 100000 == 0:
+                batch_data = load_many_and_report(heart_collection, batch_data, step)
+            else:
+                batch_data += [line_data]
+        step += 1
+    batch_data = load_many_and_report(heart_collection, batch_data, step)
+    print('Finished loading data.')
+def load_many_and_report(coll, batch_data, step):
+    coll.insert_many(batch_data)
+    print('%d lines loaded...' % step)
+    batch_data = []
+    return batch_data
+import_content("/Users/Vamshi/Desktop/GMU/AIT-614/project/heart.csv")
